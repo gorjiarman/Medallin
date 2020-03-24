7809cdfb6f9ea3737873648ffaa70846bb7f0cec
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2020-03-24 10:56:33 +0430

    Unused admin configuration removed.

--- predicting/admin.py
+++ predicting/admin.py
@@ -19,12 +19,6 @@ class DiseaseFrequencyAdmin(admin.ModelAdmin):
     list_editable = ('frequency', )
 
 
-class TransactionAdmin(admin.ModelAdmin):
-    list_display = ('concept', 'locale', 'string')
-    list_editable = ('string', )
-    list_filter = ('concept', 'locale')
-
-
 class ConditionAdmin(admin.ModelAdmin):
     list_display = ('concept', 'condition', 'factor')
     list_editable = ('factor', )
@@ -33,5 +27,4 @@ class ConditionAdmin(admin.ModelAdmin):
 
 admin.site.register(models.Concept, ConceptAdmin)
 admin.site.register(models.DiseaseFrequency, DiseaseFrequencyAdmin)
-# admin.site.register(models.Translation, TransactionAdmin)
 admin.site.register(models.Condition, ConditionAdmin)
