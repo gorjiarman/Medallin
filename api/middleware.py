5d049c35b37348300fa903859a25b8d142a0e6a5
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2020-04-08 15:17:02 +0430

    Modified the customized locale middleware.

--- api/middleware.py
+++ api/middleware.py
@@ -5,7 +5,7 @@ from django.conf import settings
 class ModifiedLocaleMiddleware(LocaleMiddleware):
 
     def process_request(self, request):
-        if request.path.startswith('/admin'):
+        if 'admin' in request.path:
             request.LANGUAGE_CODE = settings.LANGUAGE_CODE
         else:
             super().process_request(request)
