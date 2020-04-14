be3f466af8cd9e8ce46d3dedb8ff14fbdd7c18aa
Mohammad Farahmand <mohammedfarahmand98@gmail.com>
2020-04-14 15:58:23 +0430

    Replaced the admin translation enforcing middleware.

--- api/middleware.py
+++ api/middleware.py
@@ -1,11 +1,11 @@
-from django.middleware.locale import LocaleMiddleware
-from django.conf import settings
+from django.utils.deprecation import MiddlewareMixin
+from django.utils import translation
 
 
-class ModifiedLocaleMiddleware(LocaleMiddleware):
+class AdminLanguageMiddleware(MiddlewareMixin):
 
-    def process_request(self, request):
-        if 'admin' in request.path:
-            request.LANGUAGE_CODE = settings.LANGUAGE_CODE
-        else:
-            super().process_request(request)
+    @staticmethod
+    def process_request(request):
+        if request.path.startswith('/admin'):
+            translation.activate("fa-ir")
+            request.LANGUAGE_CODE = translation.get_language()
